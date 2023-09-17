5109f5b03d993b8af853c25d63def5b493e5c901
Gogi0903 <gogh.jozsef@gmail.com>
2023-09-17 19:21:53 +0200

    A textinput mezőből kiolvasott szöveget, és a választ kell törni reszponzív módon. A választ jobbra kell tartani

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import openai
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.uix.label import Label
 from Custom_Layouts import BgBoxLayout
 
-Window.size = (480, 800)
+Window.size = (540, 800)
 
 file = "./key.txt"
 
@@ -21,7 +20,6 @@ class Interface(BgBoxLayout):
 
     @staticmethod
     def openai_authenticate(keyfile):
-        # az API key-t olvassa ki a txt file-ból
         with open(keyfile) as f:
             api_key = f.read().strip('\n')
             assert api_key.startswith('sk-')
@@ -30,23 +28,14 @@ class Interface(BgBoxLayout):
     openai_authenticate(file)
 
     def question(self):
-        # a szövegmezőbe írt text-et adja vissza, reseteli a text inputot
+        # a szövegmezőbe írt text-et adja vissza
         question = self.ids.input_text.text
         self.ids.input_text.text = str()
         return question
 
-    def add_textbox_to_scr(self, text_in):
-        # az aktuális üzenetet írja ki a kijelzőre
-        new_txt_box = BgBoxLayout(hex_code='#574bc9', alpha=0.2, size_hint=(.9, None),
-                                  height=self.minimum_height)
-        label = Label(text=text_in)
-        new_txt_box.add_widget(label)
-        self.ids.conversation.add_widget(new_txt_box)
-
     def msg_to_chatgpt(self):
-        # a "küldés" gomb funkciója. Ez kommunikál a ChatGPT-vel, valamint naplózza a korábbi kérdéseket és válaszokat.
         current_question = self.question()
-        self.add_textbox_to_scr(current_question)
+        self.ids.conversation.text += f"- Én:\n{current_question}\n\n"
         self.messages.append({'role': 'user', 'content': current_question})
         self.questions.append(current_question)
         response = openai.ChatCompletion.create(
@@ -58,7 +47,7 @@ class Interface(BgBoxLayout):
         current_response = response.choices[0]['message']['content']
         self.bot_responses.append(current_response)
         self.messages.append({'role': 'assistant', 'content': current_response})
-        self.add_textbox_to_scr(current_response)
+        self.ids.conversation.text += f"- ChatGPT:\n{current_response}\n\n"
 
 
 class CloneTestApp(App):
